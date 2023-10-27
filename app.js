0981e94c94515731370014d1a4067d4ceb0374cc
BMoClay <benmorgancleveland@gmail.com>
2023-10-27 14:18:20 -0400

    good people page with working modal

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-document.addEventListener("alpine:init", () => {
-    Alpine.data("dropdown", () => ({
-        open: false,
-        toggle() {
-           this.open = !this.open;
-        },
-    }));
+// document.addEventListener("alpine:init", () => {
+//     Alpine.data("dropdown", () => ({
+//         open: false,
+//         toggle() {
+//            this.open = !this.open;
+//         },
+//     }));
 
-    Alpine.store('currentUser', {
-        username: 'ItsBen',
-        posts: ['not sure how this will be useful yet', 'still not sure but heres another post']
-    })
-})
+//     Alpine.store('currentUser', {
+//         username: 'ItsBen',
+//         posts: ['not sure how this will be useful yet', 'still not sure but heres another post']
+//     })
+// })
