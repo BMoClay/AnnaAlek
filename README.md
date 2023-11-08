760f23444b470355cd30412eabfb2d2165fbb71b
BMoClay <benmorgancleveland@gmail.com>
2023-11-07 21:06:08 -0500

    update style sheets

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Simple-Portfolio-Website
+version https://git-lfs.github.com/spec/v1
+oid sha256:9ed470a96bc977cde6a81a852ca998dd64836ab2ad3f699e4df9086b64d2cdbf
+size 26
--- app.js
+++ app.js
@@ -1,13 +1,3 @@
-// document.addEventListener("alpine:init", () => {
-//     Alpine.data("dropdown", () => ({
-//         open: false,
-//         toggle() {
-//            this.open = !this.open;
-//         },
-//     }));
-
-//     Alpine.store('currentUser', {
-//         username: 'ItsBen',
-//         posts: ['not sure how this will be useful yet', 'still not sure but heres another post']
-//     })
-// })
+version https://git-lfs.github.com/spec/v1
+oid sha256:087030492845ecd6fa0af45e0332e382e91f9450f03e0c90b45a7e67bc2f4983
+size 386
